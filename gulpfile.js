83c10723935e895326399ae5d25ce2825dac3509
Simon <simonxd@gmail.com>
2019-01-31 17:20:54 -0200

    Sets up demo

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,25 @@
 const gulp = require('gulp')
 const less = require('gulp-less')
+const browserSync = require('browser-sync').create()
 
-gulp.task('compile:less', function(){
-  return gulp.src('atoms/btn.less')
+const compileLess = () => {
+  return gulp.src('demo/**/*.less')
     .pipe(less())
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('demo'))
+    .pipe(browserSync.stream())
+}
+
+gulp.task('compile:less', compileLess)
+
+gulp.task('develop', function () {
+	browserSync.init({
+    server: {
+      baseDir: 'demo'
+    }
+  })
+
+	return gulp.watch([
+		'atoms/**/*.less',
+		'demo/**/*.less',
+	], compileLess)
 })
