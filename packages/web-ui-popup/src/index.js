68320037ae89a615c3dd542b65d34431b69ec199
Simon <simonxd@gmail.com>
2019-03-19 16:05:29 -0300

    Adds web-ui-popup component

---
+++ packages/web-ui-popup/src/index.js
@@ -0,0 +1,89 @@
+import {PropTypes} from '@orioro/web-ui-core'
+import createFocusTrap from 'focus-trap'
+import 'classlist.js'
+
+const COMPONENT_NAME = 'popup'
+
+const createInstance = (system, componentRoot, {
+	activeClass = 'active',
+	noFocusTrap = false,
+}) => {
+	const state = {
+		active: false
+	}
+
+	const focusTrap = !noFocusTrap ?
+		createFocusTrap(componentRoot) : null
+
+	const open = () => {
+		if (!state.active) {
+			console.log('open')
+			componentRoot.classList.add(activeClass)
+
+			if (focusTrap) {
+				focusTrap.activate('[autofocus]')
+			}
+
+			state.active = true
+		}
+	}
+
+	const close = () => {
+		if (state.active) {
+			console.log('close')
+			componentRoot.classList.remove(activeClass)
+
+			if (focusTrap) {
+				focusTrap.deactivate()
+			}
+
+			state.active = false
+		}
+	}
+
+	const toggle = () => {
+		if (state.active) {
+			close()
+		} else {
+			open()
+		}
+	}
+
+	document.addEventListener('keyup', e => {
+		if (state.active && e.keyCode === 27) {
+			close()
+		}
+	})
+
+	document.addEventListener('click', e => {
+		if (state.active) {
+			if (!componentRoot.contains(e.target)) {
+				// If close was called immediately,
+				// toggle triggers outside the pop-up
+				// would re-open the popup immediately,
+				// as they are outside the popup (thus closing them)
+				// and then immediately toggling their state (to open)
+				setTimeout(close, 0)
+			}
+		}
+	}, true)
+
+	return {
+		state,
+		open,
+		close,
+		toggle,
+		defaultAction: toggle,
+	}
+}
+
+export default () => {
+	return {
+		componentName: COMPONENT_NAME,
+		instancePropTypes: {
+			activeClass: PropTypes.string,
+			noFocusTrap: PropTypes.bool,
+		},
+		createInstance
+	}
+}
---
+++ packages/web-ui-popup/test/tests.spec.js
@@ -0,0 +1,7 @@
+const @orioro/webUiPopup = require('../src')
+
+describe('tests', () => {
+	test('test', () => {
+		expect(@orioro/webUiPopup()).toEqual('@orioro/web-ui-popup')
+	})
+})
