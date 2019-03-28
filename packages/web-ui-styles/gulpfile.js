188d13a1f9515cdb42e82b865a48f13e29dfd199
Simon <simonxd@gmail.com>
2019-03-28 18:32:57 -0300

    Adds gulp-size to build process

--- packages/web-ui-styles/gulpfile.js
+++ packages/web-ui-styles/gulpfile.js
@@ -1,12 +1,26 @@
+const path = require('path')
 const gulp = require('gulp')
 const less = require('gulp-less')
+const size = require('gulp-size')
 const bsInstance = require('browser-sync').create()
 
 const { series } = gulp
 
-const compileLess = () => {
-  return gulp.src('demo/**/*.less')
+const {
+  LESS_DIRS = 'demo'
+} = process.env
+
+const compileLessDemo = () => {
+
+  const LESS_GLOB = LESS_DIRS.split(',').map(dir => path.join(dir, '/**/*.less'))
+
+  console.log(`Will compile less from ${LESS_GLOB}`)
+
+  return gulp.src(LESS_GLOB, { base: 'demo' })
     .pipe(less())
+    .pipe(size({
+      showFiles: true
+    }))
     .pipe(gulp.dest('demo'))
     .pipe(bsInstance.stream())
 }
@@ -31,9 +45,9 @@ const developmentServer = developmentServerDone => {
     'defaults/**/*.less',
     'less/**/*.less',
     'demo/**/*.less',
-  ], compileLess)
+  ], compileLessDemo)
 }
 
-gulp.task('compile:less', compileLess)
+gulp.task('compile:less-demo', compileLessDemo)
 
-gulp.task('develop', series(compileLess, developmentServer))
+gulp.task('develop', series(compileLessDemo, developmentServer))
